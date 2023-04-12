a47ea0e4585273a4152e7ad4aab1b8092137bcca
William Murphy <williamjmurphy@gmial.com>
2023-04-12 14:01:13 -0500

    dev merged sith same/dev

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,6 @@ import App from './App.jsx';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> sam/dev
 const root = createRoot(document.getElementById("root"));
 
 root.render(
