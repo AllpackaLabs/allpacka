47d1bd691850a7b53da9341c1b42c6fdf9192f39
Samuel Lee <leesamuel423@gmail.com>
2023-04-13 13:21:13 -0400

    update rootLayout to use loaders
    Co-authored-by: yuanjackie1 <yuanjackie1@users.noreply.github.com>

--- client/src/layouts/rootLayout.jsx
+++ client/src/layouts/rootLayout.jsx
@@ -1,16 +1,18 @@
-import { useContext } from "react";
-import { NavLink, Outlet, useLoaderData, useParams } from "react-router-dom";
-import { userContext } from '../context';
+// import React from "react";
+import { NavLink, Outlet, useLoaderData } from "react-router-dom";
+
 const RootLayout = () => {
-  const userData = useLoaderData()
+  // const userData = useLoaderData()
 
-  const user = useContext(userContext)
-  const { setUser } = user
+  // const user = useContext(userContext)
+  // const { setUser } = user
 
-  setUser(userData)
+  // setUser(userData)
   
   // pass in trip obj
   // const [currentTrip, setCurrentTrip] = useState(null);
+	// const user = useLoaderData();
+	// const id = user._id;
 
   return (
     <div className="root-layout">
@@ -20,7 +22,7 @@ const RootLayout = () => {
           {/* <h1>I'm for testing page navigation</h1> */}
           <NavLink to='/' className='nav-link'> Login </NavLink>
           <NavLink to='/signup'className='nav-link'> Sign-up </NavLink>
-          {/* <NavLink to={id} className='nav-link'> Home </NavLink> */}
+          <NavLink to='/home_page/{id}' className='nav-link'> Home </NavLink>
           <NavLink to='/user_settings'className='nav-link'> User Settings </NavLink>
           <NavLink to='/new_trip'className='nav-link'> New Trip </NavLink>
           <NavLink to='/trip_home'className='nav-link'> Trip Home Page </NavLink>
