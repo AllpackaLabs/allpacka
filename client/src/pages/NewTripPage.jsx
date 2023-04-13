b217437109699566d73b395ead27027cbe0b6221
Samuel Lee <leesamuel423@gmail.com>
2023-04-12 21:30:07 -0400

    rough ideation of setTrip functionality
    Co-authored-by: yuanjackie1 <yuanjackie1@users.noreply.github.com>

--- client/src/pages/NewTripPage.jsx
+++ client/src/pages/NewTripPage.jsx
@@ -1,12 +1,16 @@
-import React, {useState} from "react";
-import { redirect, Form } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { useNavigate, Form } from "react-router-dom";
+import { tripContext } from "../context";
 import '../scss/NewTripPage.scss';
+
 //Will have access to userId
 const newTripPage = () => {
     const [date, setDate] = useState('');
     const [location, setLocation] = useState('');
     // const [tripType, setTripType] = useState('');
     const [tripName, setTripName] = useState('');
+		const { trip, setTrip } = useContext(tripContext);
+    const navigate = useNavigate();
 
     // handler function for the input fields
     const handleLocation = (e) => {
@@ -21,47 +25,44 @@ const newTripPage = () => {
     const handleTripName = (e) => {
         setTripName(e.target.value);
     }
-
     // this functioin send a post request to the data base to grab the _id of
     // of the new trip that was created in the database and redirects the user
     // to the tripsHome page. From there you can add items and uers, etc with put/patch requests 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // post request to server
-        fetch(`/trips/:${userId}`, {
-            method: "POST",
-            body: JSON.stringify({location: location, tripType: tripType, date: date, tripName: tripName})
-        })
-        .then(res => res.json())
-        .then((res) => {
-            // this responses should have the _id of the new trips' database
-            // id --> _id
-            // reset the state of the page to force re-render
-            setLocation('');
-            setDate('');
-            setTripType('');
-            setTripName('');
-            // grab the _id from the res -> also has
-            
-            //FixURL
-            const URL = '/TripHomePage' + res.trip_id
-            
-        // invoke prop drilled setCurrentTrip, pass in trip object
+    async function handleSubmit(e) {
+        try {
+					e.preventDefault();
+					// post request to server
+					const response = await fetch(`/trips/${user._id}`, { //make sure we are getting this user._id
+							method: "POST",
+							headers: {
+									"Content-Type": "application/json"
+							},
+							body: JSON.stringify({location, tripType, date, tripName})
+					})
+					const res = await response.json();
+					console.log('THIS IS THE RES.BODY', res);
 
-        //     redirect to the trips home page
-            return redirect(URL);
-        })
-        .catch((err) => {
+					const { _id } = res.body; //? res.trip? No se console log it
+
+					if (res) {
+						setTrip(res.trip/*something idk maybe trip*/);
+						const userUpdate = await findOneAndUpdate({_id: user._id}, {trips: [...user.trips, _id]});
+						//save() ???
+						return navigate(`/trip_home/${res.trip._id}`);
+					} else {
+						alert ('Failed To Create Trip');
+					}
+				} catch(err) {
             console.log(err);
             alert('Failed To Create Trip');
-        }); 
-    };
+        }; 
+  	};
 
 // <Form method={} action={} onSubmit={handleSubmit}> 
     return (
         // *** QUESTION: is the action leading to the correct page?
         <main className='new-trip-page'>
-            <p className='title'>       Start Planning Your Dream Trip!     </p>
+            <p className='title'>Start Planning Your Dream Trip!</p>
             <div className='container'>
                 <Form onSubmit={handleSubmit}>
                     <label>
@@ -91,3 +92,16 @@ const newTripPage = () => {
 };
 
 export default newTripPage;
+
+
+
+
+/*
+					// this responses should have the _id of the new trips' database
+					// id --> _id
+					// reset the state of the page to force re-render
+					setLocation('');
+					setDate('');
+					// setTripType('');
+					setTripName('');
+*/
