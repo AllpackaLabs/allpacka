68b5d14d7bbe266a6df9d804d77d43dc9e98f2e9
lukelauther <lukelauther1@gmail.com>
2023-04-13 13:11:59 -0400

    updated User Settings functionality

--- client/src/pages/UserHome/UserSettingsPage.jsx
+++ client/src/pages/UserHome/UserSettingsPage.jsx
@@ -8,7 +8,10 @@ const testArray = [
     {date: '1/2/2022', name: 'Yellowstone'},
     {date: '5/7/2022', name: 'Grand Canyons'},
     {date: '7/22/2022', name: 'Yosemite'},
-    {date: '3/14/2023', name: 'Smokey Mountains'},
+    {date: '9/14/2022', name: 'Smokey Mountains'},
+    {date: '1/3/2023', name: 'Glacier'},
+    {date: '2/20/2023', name: 'Teton'},
+    {date: '3/17/2023', name: 'Acadia'},
 ]
 
 
@@ -35,54 +38,53 @@ const UserSettingsPage = () => {
     // function to handle updates to the usernam or password
     function handleSubmit(e) {
         e.preventDefault()
+        window.alert('Account information has been updated.')
         // update the user state with the new information
         setUser({ username: newUserName, password: newPassword, trips: newTripsArr})
-        // send a request to the server with the updates user profile
-        fetch('/users/:_id', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: { user }
-        })
-            // parse the incoming data
-            .then(data => data.json()) 
-            // use the data
-            .then(response => {
-                console.log(response)
-            })
-            // catch any errors
-            .catch(error => console.log('Error has occured', error))
     }
     
     // function to delete a trip from a users trip
     function deleteTrip(e) {
-        // filter the array to remove the element that was clicked on
-        const updatedTrips = newTripsArr.filter(trip => trip.name !== e.target.value)
-        // update the trips array state
-        setNewTripsArr([...updatedTrips])
-        // update the user state with the new information
-        setUser({...user, trips: [...updatedTrips]})
-        fetch('/users/:_id', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: { user }
-        })
-            // parse the data
-            .then(data => data.json()) 
-            // use the data
-            .then(response => {
-                console.log(response)
-            })
-            // catch any errors
-            .catch(error => console.log('Error has occured', error))
+        if (window.confirm('Are you sure you want to delete this trip?')) {
+            // filter the array to remove the element that was clicked on
+            const updatedTrips = newTripsArr.filter(trip => trip.name !== e.target.value)
+            // update the trips array state
+            setNewTripsArr([...updatedTrips])
+            // update the user state with the new information
+            setUser({...user, trips: [...updatedTrips]})
+        } else {
+            return
+        }
     }
 
     // function to delete users account
     function handleDelete() {
-        window.confirm('Are you sure you want to delete your account?')
+        if (window.confirm('Are you sure you want to delete your account?')) {
+            // DELETE FETCH REQUEST WITH USER INFORMATION
+            // fetch('/user/:_id', {
+            //     method: 'DELETE'
+            // })
+            // <Navigate to={'/signup'} />
+        } else {
+            return 
+        }
+    }
+
+    function handleSave() {
+        window.alert('Changes have been saved.')
+        console.log('updated account info to be sent back', user)
+        // fetch('/users/:_id', {
+        //     method: 'POST', // possibly PUT?
+        //     headers: {
+        //         'Content-type': 'application/json'
+        //     },
+        //     body: { user }
+        // })
+        // .then(data => data.json())
+        // .then(response => {
+        //     console.log(response)
+        // })
+        // .catch(error => console.log('Error, ', error))
     }
 
     // function to log the current state, used for testing
@@ -94,7 +96,7 @@ const UserSettingsPage = () => {
         <main className='settings-page'>
             <div className='user-settings'>
                 <div className="update-username">
-                    {/* button used to check state when testing buttons */}
+                    {/* button to check state... used for testing */}
                     {/* <button onClick={logState}>Log State</button> */}
                     <h2>Update Username</h2>
                     <input type='text' placeholder='new username' onChange={(e) => setNewUsername(e.target.value)}/>
@@ -105,14 +107,18 @@ const UserSettingsPage = () => {
                     <input type='text' placeholder='new password' onChange={(e) => setNewPassword(e.target.value)}/>
                     <button className='password-update' onClick={handleSubmit}>update password</button>
                 </div>
+                <h2 className='trip-title'>Past Trips</h2>
                 <div className="update-trips">
-                    <h2>Past Trips</h2>
+                    {/* <h2>Past Trips</h2> */}
                     <div className="trip-log">
                         {newTripsArr.map((trip, i) => <button key={i} id={i} value={trip.name} className='trip' onClick={deleteTrip}>{trip.date} - {trip.name}</button>)}
                     </div>
                 </div>
+                <div className="save-account">
+                    <button className='save-changes' onClick={handleSave}>Save Changes</button>
+                </div>
                 <div className="delete-account">
-                    <h3>Delete Account</h3>
+                    <h2>Delete Account</h2>
                     <button className='user-update' onClick={handleDelete}>delete account</button>
                 </div>
             </div>
