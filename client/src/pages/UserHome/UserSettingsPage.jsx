d3068d452e256c630707e69e9f06aeb0e0a102ca
lukelauther <lukelauther1@gmail.com>
2023-04-13 18:25:14 -0400

    final User Settings push

--- client/src/pages/UserHome/UserSettingsPage.jsx
+++ client/src/pages/UserHome/UserSettingsPage.jsx
@@ -32,9 +32,9 @@ const UserSettingsPage = () => {
     const { user } = useContext(userContext);
     const [updateUser, setUpdateUser] = useState(user)
     // const [user, setUser] = useState(testUser)
-    const [newUserName, setNewUsername] = useState(testUser.username);
-    const [newPassword, setNewPassword] = useState(testUser.password);
-    const [newTripsArr, setNewTripsArr] = useState(testUser.trips)
+    const [newUserName, setNewUsername] = useState(updateUser.username);
+    const [newPassword, setNewPassword] = useState(updateUser.password);
+    const [newTripsArr, setNewTripsArr] = useState(updateUser.trips)
 
     const navigate = useNavigate();
 
@@ -43,7 +43,7 @@ const UserSettingsPage = () => {
         e.preventDefault()
         window.alert('Account information has been updated. Please save changes to continue.')
         // update the user state with the new information
-        setUser({ username: newUserName, password: newPassword, trips: newTripsArr})
+        setUpdateUser({ username: newUserName, password: newPassword, trips: newTripsArr})
     }
     
     // function to delete a trip from a users trip
@@ -54,7 +54,7 @@ const UserSettingsPage = () => {
             // update the trips array state
             setNewTripsArr([...updatedTrips])
             // update the user state with the new information
-            setUser({...user, trips: [...updatedTrips]})
+            setUpdateUser({...user, trips: [...updatedTrips]})
         } else {
             return
         }
@@ -64,7 +64,7 @@ const UserSettingsPage = () => {
     function handleDelete() {
         if (window.confirm('Are you sure you want to delete your account?')) {
             // DELETE FETCH REQUEST WITH USER INFORMATION
-            // fetch('/user/:_id', {  // OR `/users/${_id}`
+            // fetch('/:_id', {  // OR `/users/${_id}`
             //     method: 'DELETE'
             // })
             return navigate('/signup')
@@ -75,13 +75,13 @@ const UserSettingsPage = () => {
 
     function handleSave() {
         window.alert('Changes have been saved.')
-        console.log('updated account info to be sent back', user)
+        console.log('updated account info to be sent back', updateUser)
         // fetch('/users/:_id', {
         //     method: 'POST', // possibly PUT?
         //     headers: {
         //         'Content-type': 'application/json'
         //     },
-        //     body: { user }
+        //     body: { updateUser }
         // })
         // .then(data => data.json())
         // .then(response => {
@@ -89,19 +89,20 @@ const UserSettingsPage = () => {
         //     return navigate(`/${response.user._id}`)
         // })
         // .catch(error => console.log('Error, ', error))
+        return navigate(`/${updateUser._id}`)
     }
 
     // function to log the current state, used for testing
-    function logState() {
-        console.log('test user', updateUser)
-    }
+    // function logState() {
+    //     console.log('test user', updateUser)
+    // }
 
     return (
         <main className='settings-page'>
             <div className='user-settings'>
                 <div className="update-username">
                     {/* button to check state... used for testing */}
-                    <button onClick={logState}>Log State</button>
+                    {/* <button onClick={logState}>Log State</button> */}
                     <h2>Update Username</h2>
                     <input type='text' placeholder='new username' onChange={(e) => setNewUsername(e.target.value)}/>
                     <button className='username-update' onClick={handleSubmit}>update username</button>
